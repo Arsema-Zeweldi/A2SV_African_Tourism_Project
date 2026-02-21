ba0000525c292e752da995b4c1a5369d7b7d1f2c
samuel <cpt.macmilan24@gmail.com>
2026-02-21 11:17:23 +0300

    feat: Add new failure classes for validation and authentication errors

--- apps/mobile/lib/core/error/failures.dart
+++ apps/mobile/lib/core/error/failures.dart
@@ -21,3 +21,23 @@ class CacheFailure extends Failure {
 class NetworkFailure extends Failure {
   const NetworkFailure(super.message);
 }
+
+class InvalidEmailFailure extends Failure {
+  const InvalidEmailFailure():super('The email address is invalid format.');
+}
+
+class InvalidPasswordFailure extends Failure {
+  const InvalidPasswordFailure():super('The password must be at least 6 characters long.');
+}
+
+class InvalidFullNameFailure extends Failure {
+  const InvalidFullNameFailure():super('The full name must be at least 2 characters long.');
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure(String message) : super(message);
+}
+
+class TimeoutFailure extends Failure {
+  const TimeoutFailure(String message) : super(message);
+}
